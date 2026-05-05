e6c9b36a4cf751df99da7936c0d99fd80207eaba
Olayemi ORONKPONDE <olayemi.oronkponde@chem.loc>
2026-05-05 12:38:16 +0100

    first commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,52 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+import { useToast } from 'primevue/usetoast';
+import Button from 'primevue/button';
+import { onMounted } from 'vue';
+
+import Toast from 'primevue/toast';
+
+const toast = useToast();
+
+
+onMounted(()=>{
+   toast.add({ severity: 'success', summary: 'Success Message', detail: 'Message Content', life: 3000 });
+})
+
+
+</script>
+
+<template>
+    <Toast />
+    <Head title="Dashboard" />
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                Dashboard
+            </h2>
+        </template>
+
+
+        
+
+        <div class="py-12">
+
+
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
+                >
+                    <div class="p-6 text-gray-900">
+                        You're logged in!
+                        <Button label="Submit" />
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
