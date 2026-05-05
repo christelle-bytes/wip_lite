2dee2dfcc72c1bafa415f6cfeba097feea1bf6a9
Halil kolawolé BANGBOLA <halil.bangbola@chem.loc>
2026-05-05 19:35:36 +0100

    Modification

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -3,14 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Campaign extends Model
 {
-    protected $fillable = ['name','description','start_date','end_date','status'];
-
-     public function logs()
-{
-    return $this->morphMany(ActivityLog::class, 'model');
-}
+    protected $fillable = ['name', 'description', 'start_date', 'end_date', 'status'];
+    public function assignments()
+    {
+        return $this->hasMany(Assignment::class);
+    }
+   public function employees(): HasManyThrough
+    {
+        return $this->hasManyThrough(
+            Employee::class,
+            Assignment::class,
+            'campaign_id',
+            'id',
+            'id',
+            'employee_id'
+        );
+    }
+    public function logs()
+    {
+        return $this->morphMany(ActivityLog::class, 'model');
+    }
 
 }
