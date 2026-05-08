106503c9a482d6397d2f540f117e058ff75e706e
Theodore <limatheodore09@gmail.com>
2026-05-08 11:43:29 +0100

    time ok

--- database/seeders/PositionSeeder.php
+++ database/seeders/PositionSeeder.php
@@ -2,32 +2,42 @@
 
 namespace Database\Seeders;
 
-
 use App\Models\Position;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PositionSeeder extends Seeder
 {
-
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
-
+        // Supprime toutes les positions existantes pour garantir qu'il n'y en ait QUE 4
+        Position::query()->delete();
 
         $positions = [
-            ['name' => 'Ressource Humaine', 'code' => 'RH'],
-            ['name' => 'Chef Plateau', 'code' => 'CP'],
-            ['name' => 'Superviseur', 'code' => 'SUP'],
-            ['name' => 'Teleconseiller', 'code' => 'TC'],
+            [
+                'name' => 'Ressource Humaine',
+                'code' => 'RH'
+            ],
+            [
+                'name' => 'Chef Plateau',
+                'code' => 'CP'
+            ],
+            [
+                'name' => 'Superviseur',
+                'code' => 'SUP'
+            ],
+            [
+                'name' => 'Teleconseiller',
+                'code' => 'TC'
+            ],
         ];
 
-        foreach ($positions as $pos) {
-            Position::firstOrCreate(['code' => $pos['code']], $pos);
+        foreach ($positions as $position) {
+            Position::create($position);
         }
 
+        $this->command->info('✅ Positions initialisées avec succès (4 positions uniquement)');
     }
 }
